691aa088a393286863c56105d5a5e146dffaf659
Teejsh <insanebreed@gmail.com>
2024-08-25 17:00:53 -0400

    Added more functionality to the index.js and general.js files

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,25 +13,42 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+    res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let book_keys = Object.keys(books);
+  let filtered_book = {}
+  for (const key of book_keys) {
+    if(books[key].author.match(author)) {
+        filtered_book = books[key];
+    }
+  }
+  res.send(filtered_book);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  let book_keys = Object.keys(books);
+  let filtered_book = {}
+  for (const key of book_keys) {
+  if(books[key].title.match(title)) {
+        filtered_book = books[key];
+    }
+  }
+  res.send(filtered_book);
 });
 
 //  Get book review
