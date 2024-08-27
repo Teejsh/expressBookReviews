b4ccb997d97fa90c3e163650a29d3263dfafadb7
Teejsh <insanebreed@gmail.com>
2024-08-26 20:16:04 -0400

    Added more functionality to auth_users.js and completed all tasks

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,22 +7,83 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+    return userswithsamename.length > 0;
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
+const authenticatedUser = (username,password)=> { //returns boolean
 //write code to check if username and password match the one we have in records.
+    let validusers = users.filter((user) => {
+        return user.username === username && user.password === password;
+    });
+    return validusers.length > 0;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if (!username || !password) {
+    return res.status(404).json({ message: "Error logging in" });
+  }
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 60 });
+    req.session.authorization = {
+      accessToken, username
+    };
+    return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({ message: "Invalid Login. Check username and password" });
+  }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let book_keys = Object.keys(books);
+    const new_books = JSON.stringify(books);
+    let filtered_book = {}
+    let filtered_keys = book_keys.filter((isbn) => isbn === req.params.isbn);
+    
+    if (filtered_keys.length > 0) {
+        // Select the first matching user and update attributes if provided
+        filtered_book = books[filtered_keys[0]];
+        //console.log(filtered_book)
+
+        let review = req.query.review;    
+        if (review) {
+            filtered_book["reviews"]["review"] = review
+        }
+        
+        
+        books = JSON.parse(new_books);
+    
+        res.send(`Review with the isbn ${req.params.isbn} updated.`);
+    } else {
+        // Send error message if no book found
+        res.send("Unable to find book!");
+    }
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    let book_keys = Object.keys(books);
+    const new_books = JSON.stringify(books);
+    filtered_keys = book_keys.filter((isbn) => isbn === req.params.isbn);
+
+    if (filtered_keys.length > 0) {
+        delete books[filtered_keys[0]];
+               
+        // Send a success message as the response, indicating the book has been deleted
+        res.send(`Book with the isbn ${req.params.isbn} deleted.`);
+    } else {
+        // Send error message if no book found
+        res.send("Unable to find book!");
+    }
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,11 +3,24 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
+
+let url = 'https://towaojosipe-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/'
 
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username && password) {
+    if (!isValid(username)) {
+      users.push({ "username": username, "password": password });
+      return res.status(200).json({ message: "User successfully registered. Now you can login" });
+    } else {
+      return res.status(404).json({ message: "User already exists!" });
+    }
+  }
+  return res.status(404).json({ message: "Unable to register user." });
 });
 
 // Get the book list available in the shop
@@ -20,7 +33,7 @@ public_users.get('/',function (req, res) {
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
-    res.send(books[isbn]);
+    return res.send(books[isbn]);
  });
   
 // Get book details based on author
@@ -34,7 +47,7 @@ public_users.get('/author/:author',function (req, res) {
         filtered_book = books[key];
     }
   }
-  res.send(filtered_book);
+  return res.send(filtered_book);
 });
 
 // Get all books based on title
@@ -48,13 +61,54 @@ public_users.get('/title/:title',function (req, res) {
         filtered_book = books[key];
     }
   }
-  res.send(filtered_book);
+  return res.send(filtered_book);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+    res.send(books[isbn].reviews);
 });
 
+async function getData() {
+    try {
+        let result = await axios.get(url)
+        console.log(result.data);
+    } catch (error) {
+        console.error('Error getting data', error);
+    }
+}
+
+async function getData(isbn) {
+    try {
+        let result = await axios.get(`${url}/isbn/${isbn}`);
+        console.log(result.data);
+    } catch (error) {
+        console.error('Error getting data', error);
+    }
+}
+
+async function getDataByAuthor(author) {
+    try {
+        let result = await axios.get(`${url}/author/${author}`);
+        console.log(result.data);
+    } catch (error) {
+        console.error('Error getting data', error);
+    }
+}
+
+async function getDataByTitle(title) {
+    try {
+        let result = await axios.get(`${url}/title/${title}`);
+        console.log(result.data);
+    } catch (error) {
+        console.error('Error getting data', error);
+    }
+}
+
+getDataByTitle("The Divine Comedy");
+
 module.exports.general = public_users;
+
+
